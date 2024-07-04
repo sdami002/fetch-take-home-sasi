7a92b7a2817835107faf525d03c2041ed74e13b8
dsasi876 <sdami002@ode.edu>
2024-07-04 07:35:42 -0400

    stash

--- etl/data_transformer.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-def mask_pii(data):
-    for record in data:
-        if 'device_id' in record:
-            record['masked_device_id'] = hashlib.sha256(record['device_id'].encode()).hexdigest()
-        if 'ip' in record:
-            record['masked_ip'] = hashlib.sha256(record['ip'].encode()).hexdigest()
-    return data
